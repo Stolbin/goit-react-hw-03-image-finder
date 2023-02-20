dc69b00ae7db3c758fb12a9de7fd641ca1853a6a
Stolbin <109080579+Stolbin@users.noreply.github.com>
2023-02-20 18:12:30 +0200

    upd task

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import { Conteiner, Text } from './App.styled';
+
+class App extends Component {
+  render() {
+    return (
+      <Conteiner>
+        <Text>React homework template</Text>
+      </Conteiner>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const Conteiner = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Text = styled.p`
+  font-size: 30px;
+  font-weight: 600;
+  color: black;
+`;
