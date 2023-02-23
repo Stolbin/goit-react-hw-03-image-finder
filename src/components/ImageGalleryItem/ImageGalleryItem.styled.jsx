053d7eef0278e8c04f3c2dadb2d007fbc0e06c8a
Stolbin <109080579+Stolbin@users.noreply.github.com>
2023-02-23 14:45:10 +0200

    Update ImageGalleryItem.styled.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.styled.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.styled.jsx
@@ -7,9 +7,10 @@ export const ImageGallery = styled.img`
   border-radius: 4px;
   object-fit: cover;
   box-shadow: 2px 2px 4px #000000;
-  transition: scale 0.25s linear;
+  transition: scale 0.25s linear, box-shadow 0.25s linear;
   &:hover {
     scale: 1.02;
-    transition: scale 0.25s linear;
+    box-shadow: 0px 0px 8px #000000;
+    transition: scale 0.25s linear, box-shadow 0.25s linear;
   }
 `;
