653021babc34a36ca2e12d8021da58c9d484dc5a
Stolbin <109080579+Stolbin@users.noreply.github.com>
2023-02-28 12:23:05 +0200

    Update Searchbar.styled.jsx

--- src/components/Searchbar/Searchbar.styled.jsx
+++ src/components/Searchbar/Searchbar.styled.jsx
@@ -21,19 +21,26 @@ export const FormSearchbar = styled.form`
   display: flex;
   align-items: center;
   width: 100%;
+  height: 44px;
   max-width: 600px;
   background-color: #fff;
   border-radius: 5px;
   overflow: hidden;
   box-shadow: 1px 2px 3px #000000;
+  @media (min-width: 768px) {
+    width: 60%;
+  }
+  @media (min-width: 1200px) {
+    width: 100%;
+  }
 `;
 export const BtnSearchbar = styled.button`
   display: inline-block;
   width: 50px;
-  height: 44px;
+  height: 100%;
   border: none;
   color: #708090;
-  background-size: 40%;
+  background-size: 80%;
   background-repeat: no-repeat;
   background-position: center;
   opacity: 0.6;
@@ -41,6 +48,7 @@ export const BtnSearchbar = styled.button`
   outline: none;
   transition: opacity 250ms cubic-bezier(0.4, 0, 0.2, 1);
   &:hover {
+    scale: 1.02;
     opacity: 1;
     transition: opacity 0.25s cubic-bezier(0.4, 0, 0.2, 1);
   }
